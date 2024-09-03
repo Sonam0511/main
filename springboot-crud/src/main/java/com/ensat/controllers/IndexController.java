15668103f46354a90a24e1138a443edede84a88b
Sonam0511 <sonamsingh3351@gmail.com>
2024-09-03 20:10:21 +0530

    commit

---
+++ springboot-crud/src/main/java/com/ensat/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.ensat.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    String index() {
+        return "index";
+    }
+
+}
